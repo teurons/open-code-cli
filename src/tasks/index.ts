5f3bfbec24a53d3f3a4c04ab2d6cc701e167bb85
Rajiv Seelam <rajiv@betalectic.com>
2025-05-01 14:48:56 +0530

    Add write task and update shadcn commands with --yes flag

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -4,6 +4,7 @@ import { NpmCommandTask } from './npm-cmd';
 import { NpmInstallTask } from './npm-install';
 import { GhFetchTask } from './gh-fetch';
 import { EditJsonTask } from './edit-json';
+import { WriteTask } from './write';
 import { createTaskRegistry } from './registry';
 import { TaskRunner } from './runner';
 import { Task, TaskConfig, TaskContext, TaskRegistry, WorkflowConfig } from './types';
@@ -20,6 +21,7 @@ const createDefaultTaskRegistry = (): TaskRegistry => {
   registry.registerTask('npm_install', new NpmInstallTask());
   registry.registerTask('gh_fetch', new GhFetchTask());
   registry.registerTask('edit_json', new EditJsonTask());
+  registry.registerTask('write', new WriteTask());
   
   return registry;
 };
