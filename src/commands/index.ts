eb174b04d4cb981f3566928807060392d27faf11
Rajiv Seelam <rajiv@betalectic.com>
2025-05-06 11:35:30 +0530

    chore: Remove demo and utility commands (greeting, prompt, serialize)

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,5 @@
-import * as greeting from './greeting'
-import * as prompt from './prompt'
 import * as workflow from './workflow'
-import * as serialize from './serialize'
 import * as initOpenRouter from './init-open-router'
 import * as chooseWorkflow from './choose-workflow'
 
-export const commands = [greeting, prompt, workflow, serialize, initOpenRouter, chooseWorkflow]
+export const commands = [workflow, initOpenRouter, chooseWorkflow]
